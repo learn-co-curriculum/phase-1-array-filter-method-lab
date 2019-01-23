11d8360ca35f2f1e451745095b6dc6f4d544fdd1
Kellye Greene <kellyecodes@gmail.com>
2019-01-23 18:38:23 -0500

    Fixes tests adds solution

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-function findMatching (list, name) {
-  return list.filter(function (driverName) {
-    return driverName.toLowerCase() === name.toLowerCase();
-  });
-}
+const tutorials = [
+  'what does the this keyword mean?', 
+  'What is the Contutorialuctor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
+];
 
-function fuzzyMatch (list, partialName) {
-  let lengthOfName = partialName.length;
-  return list.filter(function (driverName) {
-    return driverName.slice(0, lengthOfName) === partialName;
-  });
-}
-
-function matchName (list, name) {
-  return list.filter(function (driver) {
-    return driver.name.toLowerCase() === name.toLowerCase();
-  });
-}
+const titleCased = tutorials.map(function (tutorial) {
+  tutorial = tutorial.split(' ');
+  for (var i = 0; i < tutorial.length; i++) {
+    tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+  }
+  return tutorial.join(' ');
+});
