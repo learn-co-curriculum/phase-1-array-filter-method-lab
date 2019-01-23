f0816b0c402099f732a8221774159f33a119a708
Kellye Greene <kellyecodes@gmail.com>
2019-01-23 12:35:43 -0500

    Adds solution

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
