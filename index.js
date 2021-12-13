b4f973a882edd229c5469e948dfc6c7d39005138
Gracie <m.graciemcguire@gmail.com>
2021-12-13 14:03:56 -0500

    adds solution

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(source, sought) {
+  return source.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+  );
+}
+
+function fuzzyMatch(source, testString) {
+  return source.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(source, soughtName) {
+  return source.filter((record) => record.name === soughtName);
+}
