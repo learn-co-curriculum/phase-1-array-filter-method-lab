f21a8a218c228b6da45074cfc6538b6b8d0f72d7
Catherine_Vuthi <vuthi.esther@gmail.com>
2022-04-12 09:50:02 +0300

    commit tree 84fc7685dacb3231a6bf3c59ac92d6318946ac5c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter(driver => str.toLowerCase() === driver.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(driver => driver.startsWith(letter));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+
