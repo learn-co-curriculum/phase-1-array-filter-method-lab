c7baa5cd45fb4b0da5124eeb3c21e3dad66edd61
Abdullah <abdna59@gmail.com>
2022-11-06 17:32:18 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution here
+const driversNames = ["Bobby", 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching( driversNames, driverName ) {
+   return driversNames.filter( ( driver ) => {
+      return driver.toLowerCase() === driverName.toLowerCase(); 
+  })
+}
+
+console.log(findMatching(driversNames,"Bobby"))
+
+
+function fuzzyMatch (arr , string){
+   let newArr = [];
+
+   if (arr.map((item) => item.substring(0, 2) === string)) {
+     return (newArr = arr.filter((item) => item.substring(0, 2) === string));
+   }
+ }
+
+console.log(fuzzyMatch(driversNames,"y"));
+
+const drivers = [
+   {
+     name: 'Bobby',
+     hometown: 'Pittsburgh' },
+   {
+     name: 'Sammy',
+     hometown: 'New York' } ,
+   {
+     name: 'Sally',
+     hometown: 'Cleveland' },
+   {
+     name: 'Annette',
+     hometown: 'Los Angeles' },
+   {
+     name: 'Bobby',
+     hometown: 'Tampa Bay' }
+ ];
+
+function matchName (arr1 , string){
+   const match = arr1.filter(obj => {
+      if(obj.name === string){
+         return obj;
+      }
+   })
+   return match;
+}
+
+console.log(matchName(drivers, "Bobby"))
